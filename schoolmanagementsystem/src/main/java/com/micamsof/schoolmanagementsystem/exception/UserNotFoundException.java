f3a39706a73105aab2218bde7727aaf163a76d84
Mitracles <89137531+Mitracles@users.noreply.github.com>
2021-11-19 15:39:49 +0800

    Delete schoolmanagementsystem/src/main/java/com/micamsof/schoolmanagementsystem/exception directory

--- schoolmanagementsystem/src/main/java/com/micamsof/schoolmanagementsystem/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.micamsof.schoolmanagementsystem.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
