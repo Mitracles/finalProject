f7064d7973c2e2a4e063dede304e409dcacd7e69
Mitracles <michaelmitra45@gmail.com>
2021-11-19 15:38:11 +0800

    added exception

---
+++ schoolmanagementsystem/src/main/java/com/micamsof/schoolmanagementsystem/service/StudentService.java
@@ -0,0 +1,42 @@
+package com.micamsof.schoolmanagementsystem.service;
+
+import com.micamsof.schoolmanagementsystem.exception.UserNotFoundException;
+import com.micamsof.schoolmanagementsystem.model.Student;
+import com.micamsof.schoolmanagementsystem.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class StudentService {
+    private final StudentRepo studentRepo;
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    public Student addStudent(Student student){
+        student.setStudentNumber(UUID.randomUUID().toString());
+        return studentRepo.save(student);
+    }
+
+    public List<Student> findAllStudents(){
+        return studentRepo.findAll();
+    }
+
+    public Student updateStudent(Student student){
+        return studentRepo.save(student);
+    }
+
+    public Student findStudentById(Long id){
+        return studentRepo.findStudentById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteStudent(Long id){
+        studentRepo.deleteStudentById(id);
+    }
+}
