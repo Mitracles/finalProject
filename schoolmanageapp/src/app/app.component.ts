0fdb729c14a68946a6ac81ab23462d09178deb3b
SofiaSum <89143535+SofiaSum@users.noreply.github.com>
2021-11-20 11:56:27 +0800

    commit

--- schoolmanageapp/src/app/app.component.ts
+++ schoolmanageapp/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Student } from "./student";
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'schoolmanageapp';
+export class AppComponent implements OnInit{
+  public students: Student[] = [];
+
+  constructor(private studentService: StudentService){}
+
+  ngOnInit(){
+    this.getStudent();
+  }
+  
+  public getStudent(): void{
+    this.studentService.getStudent().subscribe(
+      (response: Student[]) =>{
+        this.students = response;
+        console.log(this.students);
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
 }
+
+public onOpenModal(student: Student, mode: string): void{
+  const container = document.getElementById('main-container');
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.style.display='none'; 
+  button.setAttribute('data-toggle', 'modal');
+  if(mode=='add'){
+    button.setAttribute('data-target', 'addStudentModal');
+  }
+  if(mode=='edit'){
+    button.setAttribute('data-target', '#updateStudentModal');
+  }
+  if(mode=='delete'){
+    button.setAttribute('data-target', '#deleteStudentModal');
+  }
+  container?.appendChild(button);
+  button.click();
+}
+
+
+}
+ 
---
+++ schoolmanageapp/src/app/student.ts
@@ -0,0 +1,10 @@
+export interface Student{
+    id: number;
+    name:string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    studentCode: string;
+    
+}
