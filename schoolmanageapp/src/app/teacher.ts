1bbf76cb30858ef272e2df1e88c9f7340b342c8c
SofiaSum <89143535+SofiaSum@users.noreply.github.com>
2021-11-24 22:17:37 +0800

    commit

---
+++ schoolmanageapp/src/app/teacher.ts
@@ -0,0 +1,9 @@
+export interface Teacher{
+    id: number;
+    teachername: string;
+    teacheremail: string;
+    teachercontact: string;
+    teacherimageUrl: string;
+    facultyNumber: string;
+    
+}
