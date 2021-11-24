fb361604cc572931ff69679eb8ca0f08e2551d60
Mitracles <89137531+Mitracles@users.noreply.github.com>
2021-11-24 22:34:22 +0800

    updated app components

--- schoolmanageapp/src/app/app-routing.module.ts
+++ schoolmanageapp/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'teacher',
+    component: TeacherComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [TeacherComponent,StudentComponent]
--- schoolmanageapp/src/app/app.component.ts
+++ schoolmanageapp/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Student } from "./student";
+import { StudentService } from './student.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'schoolmanageapp';
+export class AppComponent implements OnInit{
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(){
+  }
+
+  ngOnInit(){;
+  }
+  
+
 }
+ 
